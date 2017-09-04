06533b14236696a4b76f8f2acd96ea6ba86f1222
minglu <minglu@localhost.localdomain>
2017-09-04 03:47:54 -0400

    final

--- install_spark.sh
+++ install_spark.sh
@@ -2,41 +2,38 @@
 #install Spark on CentOS 7
 
 ## install java
-cd /tmp
-wget http://ftp.wsisiz.edu.pl/pub/pc/pozyteczne%20oprogramowanie/java/jdk-8u131-linux-x64.rpm
-sudo rpm -ivh /tmp/jdk-8u131-linux-x64.rpm
-sudo rm /tmp/jdk-8u131-linux-x64.rpm
-## append /etc/profile 
+cd /home/minglu
+wget http://ftp.wsisiz.edu.pl/pub/pc/pozyteczne%20oprogramowanie/java/jdk-8u144-linux-x64.rpm
+sudo rpm -ivh /home/minglu/jdk-8u131-linux-x64.rpm
+rm /home/minglu/jdk-8u131-linux-x64.rpm
 
-echo 'JAVA_HOME=/usr/java/java' >> ~/.bashrc
-echo 'JRE_HOME=$JAVA_HOME/jre'  >> ~/.bashrc echo 'CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JRE_HOME/lib/rt.jar'  >> ~/.bashrc
-echo 'PATH=$PATH:$JAVA_HOME/bin'  >> ~/.bashrc
+##install scala
 
-source ~/.bashrc
-java -version
-
-sudo yum install wget -y
+yum install wget -y
 wget http://downloads.typesafe.com/scala/2.11.11/scala-2.11.11.tgz
 tar xvf scala-2.11.11.tgz
-sudo mv scala-2.11.11 /usr/lib
-sudo ln -s /usr/lib/scala-2.11.11 /usr/lib/scala
-echo 'PATH=$PATH:/usr/lib/scala/bin' >> ~/.bashrc
-source ~/.bashrc
-scala -version
+
+#install spark
 
 wget http://d3kbcqa49mib13.cloudfront.net/spark-2.2.0-bin-hadoop2.7.tgz
 tar xvf spark-2.2.0-bin-hadoop2.7.tgz
 
-export SPARK_HOME=$HOME/spark-2.2.0-bin-hadoop2.7 >> ~/.bashrc
-export PATH=$PATH:$SPARK_HOME/bin >> ~/.bashrc
-
+##開啟port
 firewall-cmd --permanent --zone=public --add-port=6066/tcp
 firewall-cmd --permanent --zone=public --add-port=7077/tcp
 firewall-cmd --permanent --zone=public --add-port=8080-8081/tcp
 firewall-cmd --reload
 
-echo 'export PATH=$PATH:/usr/lib/scala/bin' >> ~/.bash_profile
-echo 'export SPARK_HOME=$HOME/spark-2.2.0-bin-hadoop2.7' >> ~/.bash_profile
-echo 'export PATH=$PATH:$SPARK_HOME/bin' >> ~/.bash_profile
-source ~/.bash_profile
-##execute  /etc/profile -> (~/.bash_profile | ~/.bash_login | ~/.profile) -> ~/.bashrc -> /etc/bashrc -> ~/.bash_logout
+
+
+echo 'export SCALA_HOME=/home/minglu/scala-2.11.11' >> ~/.bashrc
+echo 'export PATH=$SCALA_HOME/bin:$PATH' >> ~/.bashrc
+echo 'export SPARK_HOME=/home/minglu/spark-2.2.0-bin-hadoop2.7' >> ~/.bashrc
+echo 'export PATH=$SPARK_HOME/bin:$PATH' >> ~/.bashrc
+echo 'export JAVA_HOME=/home/minglu/jdk1.8.0_144' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+
+
+java -version
+scala -version
